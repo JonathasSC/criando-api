b747bf1d7246a71bf026f352efcfa714c3e4cb5c
Jonathas Dos Santos <89261014+JonathasSC@users.noreply.github.com>
2022-12-29 13:29:10 -0300

    API 2.0 finalizada

--- index.py
+++ index.py
@@ -14,7 +14,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-filmes = [
+movies = [
     {
         'id': 1,
         'titulo': 'Os Vingadores - The Avengers',
@@ -44,27 +44,42 @@ filmes = [
 
 # Consultar (todos)
 @app.route('/filmes', methods=['GET'])
-def obter_filmes():
-    return jsonify(filmes)
+def GetMovieList():
+    return jsonify(movies)
 
 
 # Consultar filmes por ID
 @app.route('/filmes/<int:id>', methods=['GET'])
-def obter_filme_por_id(id):
-    for filme in filmes:
-        if filme.get('id') == id:
-            return jsonify(filme)
+def GetMovieByID(id):
+    for movie in movies:
+        if movie.get('id') == id:
+            return jsonify(movie)
 
 
 # Editar
 @app.route('/filmes/<int:id>', methods=['PUT'])
-def editar_filme_por_id(id):
+def MovieEditByID(id):
 
-    for indice,filme in enumerate (filmes):
-        if filme.get('id') == id:
-            filmes[indice].update(request.get_json())
-            return jsonify(filmes[indice])
+    for indice,movie in enumerate (movies):
+        if movie.get('id') == id:
+            movies[indice].update(request.get_json())
+            return jsonify(movies[indice])
 
+# Criar 
+@app.route('/filmes', methods=['POST'])
+def AddMovie():
+	new_movie = request.get_json()
+	movies.append(new_movie)
+
+	return jsonify(movies)
+	
 # Excluir
+@app.route('/filmes/<int:id>', methods=['DELETE'])
+def DeleteMovie(id):
+	for indice, movie in enumerate(movies):
+		if movie.get('id') == id:
+			del movies[indice]
+			
+	return jsonify(movies)
 
 app.run(port=5000,host='localhost',debug=True)
